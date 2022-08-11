d02a9eb9a85454b090a45f3b98686332c1105634
suleyman <jdksuleyman@gmail.com>
2022-08-11 13:53:28 +0300

    database işlemleri eklendi

---
+++ BookStore/Controllers/BookController.cs
@@ -0,0 +1,90 @@
+﻿using BookStore.Entity;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BookStore.Controllers
+{
+    [ApiController]
+    [Route("[controller]s")]
+    public class BookController : Controller
+    {
+        private static List<Book> BookList = new List<Book>()
+        {
+            new Book
+            {
+                Id = 1,
+                Title = "Lean Startup",
+                GenreId = 1,
+                PageCount = 200,
+                PublishDate = new System.DateTime(2001, 06, 20)
+            },
+            new Book
+            {
+                Id = 2,
+                Title = "Heidi",
+                GenreId = 2,
+                PageCount = 250,
+                PublishDate = new System.DateTime(2002, 02, 15)
+            },
+            new Book
+            {
+                Id = 3,
+                Title = "Harry Potter",
+                GenreId = 2,
+                PageCount = 500,
+                PublishDate = new System.DateTime(2005, 02, 10)
+            }
+        };
+
+        [HttpGet]
+        public List<Book> GetBooks()
+        {
+            var bookList = BookList.OrderBy(x => x.Id).ToList<Book>();
+            return bookList;
+        }
+
+        [HttpGet("{id}")]
+        public Book GetById(int id)
+        {
+            var book = BookList.Where(x => x.Id == id).SingleOrDefault();
+            return book;
+        }
+        [HttpPost]
+        public IActionResult AddBook([FromBody] Book newBook)
+        {
+            var book = BookList.SingleOrDefault(x => x.Title == newBook.Title);
+            if (book is not null)
+                return BadRequest();
+            BookList.Add(newBook);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateBook(int id, [FromBody] Book updatedBook)
+        {
+            var book = BookList.SingleOrDefault(x => x.Id == id);
+
+            if (book is null)
+                return BadRequest();
+            book.GenreId = updatedBook.GenreId != default ? updatedBook.GenreId : book.GenreId;
+            book.PageCount = updatedBook.PageCount != default ? updatedBook.PageCount : book.PageCount;
+            book.PublishDate = updatedBook.PublishDate != default ? updatedBook.PublishDate : book.PublishDate;
+            book.Title = updatedBook.Title != default ? updatedBook.Title : book.Title;
+
+            return Ok();
+        }
+
+        //[HttpDelete("{id}")]
+        //public ActionResult DeleteBook(int id)
+        //{
+        //    var book = BookList.SingleOrDefault(x => x.Id == id);
+        //    if (book is null)
+        //    {
+        //        return BadRequest();
+        //    }
+        //}
+    }
+    
+}
+
